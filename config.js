a9a693dbcfd721cf045aae115784c5dbf0d3ea6e
hamidkhan <hamidkhan56@gmail.com>
2023-01-19 15:02:20 +0500

    done

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
-var mongoose = require ('mongoose');
-// mongoose.set('strictQuery',false)
-mongoose.connect('mongodb://localhost:27017/users');
+const mongoose = require ('mongoose');
+
+mongoose .connect("mongodb://localhost:27017/users", {
+        useNewUrlParser: true,
+           })   
+ .then(() => console.log("Database connected!"))
+ .catch(err => console.log(err));
