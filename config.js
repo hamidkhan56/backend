c9d0b4b84b3bd70e95e00d0a3699a78a0990b56f
hamidkhan <hamidkhan56@gmail.com>
2023-01-19 15:42:41 +0500

    done

--- config.js
+++ config.js
@@ -3,8 +3,7 @@ require('dotenv').config()
 console.log(process.env.DATABASE_URL)
 mongoose .connect(process.env.DATABASE_URL, {
         useNewUrlParser: true,
-        
-    
+        bufferCommands: false 
            })   
  .then(() => console.log("Database connected!"))
  .catch(err => console.log(err));
