46f8a400ad1221dd2fcfae16888c7455fa8543f6
hamidkhan <hamidkhan56@gmail.com>
2023-01-19 15:12:56 +0500

    done

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const mongoose = require ('mongoose');
-
-mongoose .connect("mongodb://localhost:27017/users", {
+const db = "mongodb://localhost:27017/users"
+mongoose .connect(db, {
         useNewUrlParser: true,
            })   
  .then(() => console.log("Database connected!"))
